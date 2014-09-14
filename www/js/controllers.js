222bcba83186c5d59b25cb6af313153ad28d7e3b
Hai Nguyen <hmn91733@gmail.com>
2014-09-13 17:47:12 -0700

    Add ShakeDetection service

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,45 +1,16 @@
 angular.module('Booze.controllers', [])
 
-.controller('MainCtrl', function($scope, $cordovaDeviceMotion, $cordovaDialogs) {
-  $scope.getAcceleration = function () {
-    $cordovaDeviceMotion.getCurrentAcceleration().then(function(result) {
-      // Success!
-    }, function(err) {
-      // An error occured. Show a message to the user
-    });
-  };
+.controller('MainCtrl', function($scope, $cordovaDeviceMotion, $cordovaDialogs, ShakeDetection) {
 
-  $scope.watchAcceleration = function () {
-    // Update every 3 seconds for 1 minute
-    var options = {
-      maximumAge: 3000,
-      timeout: 60 * 1000,
-      enableHighAccuracy: true
-    };
+  function myCallBack() {
+    $cordovaDialogs.alert('It shaked!');
+  }
 
-    watch = $cordovaDeviceMotion.watchAcceleration(options);
+  $scope.watchShake = function() {
+    ShakeDetection.startWatch(myCallBack);
+  }
 
-    watch.promise.then(
-      function() {/* unused */},
-      function(err) {},
-      function(acceleration) {
-        $cordovaDialogs.alert('Acceleration X: ' + acceleration.x + '\n' +
-           'Acceleration Y: ' + acceleration.y + '\n' +
-           'Acceleration Z: ' + acceleration.z + '\n' +
-           'Timestamp: '      + acceleration.timestamp + '\n');
-    });
-  };
-
-  $scope.clearWatch = function() {
-    // use watchID from watchAccelaration()
-
-    if(!watch) { return; }
-
-    $cordovaDeviceMotion.clearWatch(watch.watchId).then(function(result) {
-      // Success!
-    }, function(err) {
-      // An error occured. Show a message to the user
-
-    });
+  $scope.stopWatch = function() {
+    ShakeDetection.stopWatch();
   }
 })
--- www/js/services.js
+++ www/js/services.js
@@ -3,24 +3,69 @@ angular.module('Booze.services', [])
 /**
  * A simple example service that returns some data.
  */
-.factory('Friends', function() {
-  // Might use a resource here that returns a JSON array
+.factory('ShakeDetection', function($cordovaDeviceMotion, $cordovaDialogs) {
+  var shake = {},
+      watchId = null,
+      options = {
+        frequency: 3000
+      },
+      previousAcceleration = { x: null, y: null, z: null },
+      shakeCallBack = null;
 
-  // Some fake testing data
-  var friends = [
-    { id: 0, name: 'Scruff McGruff' },
-    { id: 1, name: 'G.I. Joe' },
-    { id: 2, name: 'Miss Frizzle' },
-    { id: 3, name: 'Ash Ketchum' }
-  ];
+    // Start watching the accelerometer for a shake gesture
+    shake.startWatch = function (onShake) {
+      if (onShake) {
+        shakeCallBack = onShake;
+      }
+      watchId = $cordovaDeviceMotion.watchAcceleration(options);
 
-  return {
-    all: function() {
-      return friends;
-    },
-    get: function(friendId) {
-      // Simple index lookup
-      return friends[friendId];
+      watchId.promise.then(
+        function() {/* unused */},
+        handleError,
+        assessCurrentAcceleration
+      );
+    };
+
+    // Stop watching the accelerometer for a shake gesture
+    shake.stopWatch = function () {
+      if (watchId !== null) {
+        $cordovaDeviceMotion.clearWatch(watchId);
+        watchId = null;
+      }
+    };
+
+    // Assess the current acceleration parameters to determine a shake
+    function assessCurrentAcceleration(acceleration) {
+      var accelerationChange = {};
+      if (previousAcceleration.x !== null) {
+        accelerationChange.x = Math.abs(previousAcceleration.x, acceleration.x);
+        accelerationChange.y = Math.abs(previousAcceleration.y, acceleration.y);
+        accelerationChange.z = Math.abs(previousAcceleration.z, acceleration.z);
+      }
+      if (accelerationChange.x + accelerationChange.y + accelerationChange.z > 30) {
+        // Shake detected
+        if (typeof (shakeCallBack) === "function") {
+          shakeCallBack();
+        }
+        shake.stopWatch();
+        setTimeout(shake.startWatch, 1000);
+        previousAcceleration = {
+          x: null,
+          y: null,
+          z: null
+        }
+      } else {
+        previousAcceleration = {
+          x: acceleration.x,
+          y: acceleration.y,
+          z: acceleration.z
+        }
+      }
     }
-  }
+
+    // Handle errors here
+    function handleError() {
+    }
+
+    return shake;
 });
